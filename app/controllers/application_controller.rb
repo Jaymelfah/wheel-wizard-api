f1c89743e511b1b23f23fade36f487e69c3cfb4d
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-03-04 07:10:42 +0000

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,21 +8,17 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate_user_if_token_present
-    
-
     token = request.headers['Authorization']&.split(' ')&.last
     return unless token
 
     payload = JsonWebToken.decode(token)
-   
+
     user_id = payload['sub']
-    
+
     @current_user = User.find_by(id: user_id)
-    
 
     render json: { error: 'Invalid or expired token' }, status: :unauthorized if @current_user.nil?
   rescue JWT::ExpiredSignature, JWT::DecodeError
-   
     render json: { error: 'Invalid or expired token' }, status: :unauthorized
   end
 
