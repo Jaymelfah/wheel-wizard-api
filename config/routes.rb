6a9cb58bbf454ac4f1938390519e4d6edcb20f0c
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-03-04 12:36:50 +0000

    Add registration controller to show message after successful sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'sessions'
+    sessions: 'sessions',
+    registrations: 'registrations'
   }
   
   namespace :api do
